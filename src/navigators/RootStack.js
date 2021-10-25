82e9869f27b4c0fbcf6c298a4b417c89ca35d67a
kirua <yo44prg@icloud.com>
2021-10-25 10:06:56 +0200

    feat: add header and bottom nav

--- src/navigators/RootStack.js
+++ src/navigators/RootStack.js
@@ -1,45 +1,55 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 // Screens
 import HomeScreen from "../screens/HomeScreen";
 import CaptureScreen from "../screens/CaptureScreen";
 import DisplayResultScreen from "../screens/DisplayResultScreen";
 
-const Stack = createNativeStackNavigator();
+import { primary_c } from "../style/theme"
+
+const Stack = createMaterialBottomTabNavigator();
 
 const RootStack = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="HomeScreen"
-        screenOptions={{
-          headerShown: false,
-          // headerTintColor: "red",
-          // headerTransparent: true,
-          // headerTitle: "",
-          // headerTitleAlign: "right",
-          // headerLeftContainerStyle: {
-          //   paddingLeft: 20,
-          // },
-        }}
+      <Stack.Navigator 
+        activeColor="#fff"
+        inactiveColor="#a8a8a4"
+        barStyle={{ backgroundColor: primary_c }}
       >
-        <Stack.Screen
-          name="HomeScreen"
+        <Stack.Screen 
+          name="Home" 
           component={HomeScreen}
-          options={{ title: "Acceuil" }}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }} 
         />
-        <Stack.Screen
+        <Stack.Screen 
           name="CaptureScreen"
-          component={CaptureScreen}
-          options={{ title: "Capture" }}
+          component={CaptureScreen} 
+          options={{
+            tabBarLabel: 'Capture',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="camera-plus" color={color} size={26} />
+            ),
+          }} 
         />
-        <Stack.Screen
-          name="DisplayResultScreen"
+        <Stack.Screen 
+          name="DisplayResultScreen" 
           component={DisplayResultScreen}
-          options={{ title: "Resultat de la capture" }}
+          options={{
+            tabBarLabel: 'Result',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="clipboard-file" color={color} size={26} />
+            ),
+          }} 
         />
       </Stack.Navigator>
     </NavigationContainer>
