4ab96a3d3de970f05da6e6f4c29e57c9493341aa
galba-rp <yaroslav.hontar@gmail.com>
2021-10-29 11:22:16 +0200

    fix:ipmort StyleSheet

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { View, Button, Platform } from 'react-native'
+import { View, Button, Platform, StyleSheet } from 'react-native'
 import PreviewScreen from './PreviewScreen';
 import { useDispatch, useSelector } from 'react-redux';
 import * as Location from 'expo-location';
