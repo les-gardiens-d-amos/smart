1d9c3aa5e161452fe6cd7cbc7311db15b2e3f8bc
galba-rp <yaroslav.hontar@gmail.com>
2021-11-04 13:30:18 +0100

    fix: add error message on clarifai or imgur error
    add redirect to " Retourner" button on error or "Amos not found"

--- src/navigators/RootStack.js
+++ src/navigators/RootStack.js
@@ -9,6 +9,7 @@ import ArchamosScreen from "../screens/ArchamosScreen";
 import AmosSingleScreen from "../screens/AmosSingleScreen";
 import DashboardScreen from "../screens/DashboardScreen";
 import DisplayResultScreen from "../screens/DisplayResultScreen";
+import PreviewScreen from "../screens/PreviewScreen";
 
 import LoginScreen from "../screens/LoginScreen";
 import { colors } from "../style/theme";
@@ -68,6 +69,11 @@ const RootStack = () => {
             name="DisplayResultScreen"
             component={DisplayResultScreen}
             options={{ title: "Capture" }}
+          />
+          <Stack.Screen
+            name="PreviewScreen"
+            component={PreviewScreen}
+            options={{ title: "Preview" }}
           />
         </Stack.Navigator>
       </NavigationContainer>
