714d054670b5587b1ec79e678023df19da567b64
guil-martin <gu.martin@yahoo.com>
2021-11-11 10:25:29 +0100

    Starting to restyle PreviewScreen

--- src/screens/PreviewScreen.js
+++ src/screens/PreviewScreen.js
@@ -1,102 +1,76 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
-import { useDispatch } from 'react-redux';
-import { setCapturedImageAction } from '../app/slices/cameraSlice';
-// import { setCapturedImageAction } from '../store/actions/CameraActions';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { useDispatch } from "react-redux";
+import { setCapturedImageAction } from "../app/slices/cameraSlice";
 import { colors } from "../style/theme";
-import DisplayResultScreen from './DisplayResultScreen';
-
+import DisplayResultScreen from "./DisplayResultScreen";
 
 const PreviewScreen = ({ image }) => {
-
   const dispatch = useDispatch();
-  const [getInfo, setGetInfo] = useState(false)
-
-  const savePhoto = () => {
-    setGetInfo(true);
-  }
+  const [getInfo, setGetInfo] = useState(false);
 
   const cancelPicture = () => {
-    dispatch(setCapturedImageAction({}))
-  }
+    dispatch(setCapturedImageAction({}));
+  };
+
+  if (getInfo) return <DisplayResultScreen />;
+
   return (
     <View style={styles.container}>
-      {getInfo ?
-        (
-          <DisplayResultScreen />
-        ) : (
-          <View style={styles.screenView}>
-            <Image
-              source={{ uri: image.path }}
-              style={{
-                flex: 1
-              }}
+      <View style={styles.screenView}>
+        <Image style={styles.img} source={{ uri: image.path }}></Image>
+        <View style={styles.imageBackgroundView}>
+          <View style={styles.buttonsContainerView}>
+            <TouchableOpacity onPress={cancelPicture} style={styles.buttons}>
+              <Text style={styles.buttonsText}>Annuler</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setGetInfo(true)}
+              style={styles.buttons}
             >
-            </Image>
-            <View style={styles.imageBackgroundView}>
-              <View style={styles.buttonsContainerView}>
-                <TouchableOpacity
-                  onPress={cancelPicture}
-                  style={styles.buttons}
-                >
-                  <Text style={styles.buttonsText}>
-                    Annuler
-                  </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={savePhoto}
-                  style={styles.buttons}
-                >
-                  <Text style={styles.buttonsText}>
-                    Confirmer
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </View>
+              <Text style={styles.buttonsText}>Confirmer</Text>
+            </TouchableOpacity>
           </View>
-        )
-      }
-
+        </View>
+      </View>      
     </View>
-
-
-  )
-}
+  );
+};
 
 export default PreviewScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   screenView: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     flex: 1,
-    width: '100%',
-    height: '100%'
+    width: "100%",
+    height: "100%",
+  },
+  img: {
+    width: "100%",
   },
   imageBackgroundView: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
     padding: 15,
-    justifyContent: 'flex-end'
+    justifyContent: "flex-end",
   },
   buttonsContainerView: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   buttons: {
     width: 130,
     padding: 15,
-    alignItems: 'center',
+    alignItems: "center",
     borderRadius: 4,
-    backgroundColor: colors.primary_c
+    backgroundColor: colors.primary_c,
   },
   buttonsText: {
-    color: '#fff',
-    fontSize: 20
-  }
+    color: "#fff",
+    fontSize: 20,
+  },
 });
